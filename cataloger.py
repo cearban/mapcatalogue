bc5d6ee4756964c6dbcb1cc9d7ea88b903ac562a
jcrone <james.crone@ed.ac.uk>
2020-03-25 16:53:49 +0000

    refactored

--- cataloger.py
+++ cataloger.py
@@ -89,6 +89,8 @@ def fetch_web_map_services_from_csw(csw):
     return wms_l
 
 
+
+
 def catalog_web_map_services(csw, query, refresh=False):
     """
     catalog a CSW
@@ -253,12 +255,100 @@ def check_wms_map_image(fn):
     return status
 
 
+def get_ogc_type(url):
+    ogc_type = None
+
+    if 'wms' in url.lower():
+        if 'getcapabilities' in url.lower():
+            ogc_type = 'WMS:GetCapabilties'
+        if 'getmap' in url.lower():
+            ogc_type = 'WMS:GetMap'
+    elif 'wcs' in url.lower():
+        if 'describecoverage' in url.lower():
+            ogc_type = 'WCS:DescribeCoverage'
+        if 'getcoverage' in url.lower():
+            ogc_type = 'WCS:GetCoverage'
+    elif 'wfs' in url.lower():
+        if 'getcapabilities' in url.lower():
+            ogc_type = 'WFS:GetCapabilities'
+        if 'getfeature' in url.lower():
+            ogc_type = 'WFS:GetFeature'
+
+    return ogc_type
+
+
+def search_csw_for_ogc_endpoints(csw_url, search_term=None, limit=True, max_records_to_search=200):
+    out_records = []
+
+    csw = CatalogueServiceWeb(csw_url)
+
+    start_pos = 0
+    max_record_default = int(csw.constraints['MaxRecordDefault'].values[0])  # i.e. 10
+
+    if search_term is not None:
+        csw_query = PropertyIsEqualTo('csw:AnyText', search_term)
+        csw.getrecords2(constraints=[csw_query], startposition=start_pos)
+    else:
+        csw.getrecords2(startposition=start_pos)
+
+    if not limit:
+        max_records_to_search = csw.results['matches']
+
+    record_id = 0
+
+    for rec in csw.records:
+        r = csw.records[rec]
+        references = r.references
+        ogc_urls = []
+        for ref in references:
+            url = ref['url']
+            ogc_url_type = get_ogc_type(url)
+            if ogc_url_type is not None:
+                ogc_urls.append([ogc_url_type, url])
+        if len(ogc_urls) > 0:
+            record_id += 1
+            for u in ogc_urls:
+                out_records.append([record_id, r.title, u[0], u[1]])
+
+    start_pos += max_record_default
+
+    while start_pos < max_records_to_search:
+        if search_term is not None:
+            csw_query = PropertyIsEqualTo('csw:AnyText', search_term)
+            csw.getrecords2(constraints=[csw_query], startposition=start_pos)
+        else:
+            csw.getrecords2(startposition=start_pos)
+
+        for rec in csw.records:
+            r = csw.records[rec]
+            references = r.references
+            ogc_urls = []
+            for ref in references:
+                url = ref['url']
+                ogc_url_type = get_ogc_type(url)
+                if ogc_url_type is not None:
+                    ogc_urls.append([ogc_url_type, url])
+            if len(ogc_urls) > 0:
+                record_id += 1
+                for u in ogc_urls:
+                    out_records.append([record_id, r.title, u[0], u[1]])
+
+        start_pos += max_record_default
+
+    return out_records
+
+
 def main():
-    #fn = '/home/james/geocrud/wms_getmaps/wms_list.csv'
-    #interrogate_wms_layers(fn)
-    for fn in glob.glob('/home/james/geocrud/wms_getmaps/*.png'):
-        status = check_wms_map_image(fn)
-        print(fn, status)
+    csw_url = 'https://ckan.publishing.service.gov.uk/csw?request=GetCapabilities&service=CSW&version=2.0.2'
+    search_term = 'Greenspace'
+
+    ogc_endpoints = search_csw_for_ogc_endpoints(csw_url, max_records_to_search=300)
+    with open('/home/james/Desktop/ogc_endpoints.csv', 'w') as outpf:
+        my_writer = csv.writer(outpf, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+        my_writer.writerow(['record_id', 'record_title', 'ogc_url_type', 'ogc_url'])
+        for e in ogc_endpoints:
+            my_writer.writerow([e[0], e[1], e[2], e[3]])
+
 
 
 if __name__ == "__main__":
