1aa88409b3c45419f99d3d67bc7d4a7761b23674
jcrone <james.crone@ed.ac.uk>
2020-03-30 16:48:46 +0100

    moved stuff into cataloger.py

--- ogc_inspector.py
+++ ogc_inspector.py
@@ -14,14 +14,12 @@ import xml
 import pprint
 import Levenshtein as lvn
 
-
 # logging.basicConfig(
 #     filename='/home/james/geocrud/wms_getmaps/wms_getmap_log.txt',
 #     filemode='w',
 #     level=logging.DEBUG,
 #     format='%(levelname)s:%(message)s'
 # )
-
 
 
 class MyBbox:
@@ -116,45 +114,6 @@ def interrogate_wms_layer(wms_url, wms_lyr_name):
                 logging.error("For WMS {} layer {}, had problems writing map to {}".format(wms_url, wms_lyr_name, out_fn))
 
 
-# def fetch_wms_layers_in_wms_catalogue():
-#     # data.gov.uk csw endpoint
-#     csw_url = 'https://ckan.publishing.service.gov.uk/csw?request=GetCapabilities&service=CSW&version=2.0.2'
-#     csw = CatalogueServiceWeb(csw_url)
-#
-#     # search for Greenspace
-#     my_query = PropertyIsEqualTo('csw:AnyText', 'Greenspace')
-#     wms_catalog_fn = catalog_web_map_services(csw, my_query)
-#
-#     wms_layers = []
-#
-#     # 1. build up the catalog of WMSs
-#     if os.path.exists(wms_catalog_fn):
-#         with open(wms_catalog_fn, 'r') as inpf:
-#             my_reader = csv.DictReader(inpf)
-#             for r in my_reader:
-#                 wms_title = r["title"]
-#                 wms_url = r['url']
-#
-#                 try:
-#                     # default timeout is 30
-#                     wms = WebMapService(wms_url, timeout=5)
-#                     #print('OK', wms_url)
-#                     #print('wms has these layers:')
-#                     print('OK', wms_url)
-#                     for wms_lyr_name in wms.contents:
-#                         wms_layers.append([wms_url, wms_lyr_name])
-#                 except (requests.RequestException, AttributeError) as ex:
-#                     print('Exception(s) raised with ', wms_url)
-#                     print(ex)
-#
-#     with open('/home/james/geocrud/wms_getmaps/wms_list.csv', 'w') as outpf:
-#         my_writer = csv.writer(outpf, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-#         my_writer.writerow(["wms_url", "lyr_name"])
-#         for i in wms_layers:
-#             my_writer.writerow([i[0], i[1]])
-
-
-
 def check_wms_map_image(fn):
     status = None
 
@@ -171,118 +130,32 @@ def check_wms_map_image(fn):
     return status
 
 
-def fetch_wms_layers():
-    fn = "/home/james/Desktop/Work/ogc_endpoints.csv"
-    c = 1
-    out_records = []
+def main():
+    c = 0
+    fn = '/home/james/geocrud/wms_layers.csv'
     if os.path.exists(fn):
         with open(fn, 'r') as inpf:
             my_reader = csv.DictReader(inpf)
             for r in my_reader:
-                if r['ogc_url_type'] != 'NotFound':
-                    record_id = r['record_id']
-                    record_title = r['record_title']
-                    ogc_url_type = r['ogc_url_type']
-                    ogc_url = r['ogc_url']
-                    if 'WMS' in ogc_url_type:
-                        if ogc_url_type == 'WMS:GetCapabilties':
-                            if c < 50:
-                                print(record_id)
-                                print(record_title)
-                                print(c, ogc_url_type, ogc_url)
-                                wms_layers = []
-                                try:
-                                    wms = WebMapService(ogc_url, timeout=5)
-                                    for wms_lyr_name in wms.contents:
-                                        wms_layers.append(wms_lyr_name)
-                                except requests.RequestException as ex:
-                                    pass
-                                    #print(ex)
-                                except AttributeError as ex:
-                                    pass
-                                    #print(ex)
-                                except xml.etree.ElementTree.ParseError as ex:
-                                    pass
-                                    #print(ex)
-                                if len(wms_layers) > 0:
-                                    for wms_layer in wms_layers:
-                                        print('\t', wms_layer)
-                                        out_records.append([record_id, record_title, ogc_url, wms_layer])
-                                else:
-                                    print("No WMS layers listed OR could not retrieve them from WMS")
-                            c += 1
-
-    if len(out_records) > 0:
-        with open("/home/james/Desktop/Work/wms_layers_by_record_title.csv", "w") as outpf:
-            my_writer = csv.writer(outpf, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-            my_writer.writerow(['record_id', 'record_title', 'ogc_url', 'wms_layer'])
-            for i in out_records:
-                my_writer.writerow([i[0], i[1], i[2], i[3]])
-
-
-def look_for_title_in_wms_layers():
-
-    d = {}
-
-    fn = "/home/james/Desktop/Work/wms_layers_by_record_title.csv"
-    if os.path.exists(fn):
-        with open(fn, "r") as inpf:
-            my_reader = csv.DictReader(inpf)
-            for r in my_reader:
-                record_id = r['record_id']
-                record_title = r['record_title']
-                ogc_url = r['ogc_url']
-                wms_layer = r['wms_layer']
-                if record_id in d:
-                    d[record_id]['wms_layers'].append([ogc_url, wms_layer])
-                else:
-                    d[record_id] = {
-                        'record_title': record_title,
-                        'wms_layers': [[ogc_url, wms_layer]],
-                        'wms_layer_for_record': None,
-                        'match_dist': None,
-                        'only_1_choice': False
-                    }
-
-    # TODO - use Levenshtein distance via https://rawgit.com/ztane/python-Levenshtein/master/docs/Levenshtein.html
-    #  we just use dist which is absolute Levenshtein distance between 2 string but maybe the other functions are
-    #   more appropriate?
-    for record_id in d:
-        record_title = d[record_id]['record_title']
-        num_wms_layers = len(d[record_id]['wms_layers'])
-        if num_wms_layers == 1:
-            d[record_id]['only_1_choice'] = True
-            d[record_id]['wms_layer_for_record'] = d[record_id]['wms_layers'][0][1]
-            d[record_id]['match_dist'] = lvn.distance(record_title.lower(), (d[record_id]['wms_layers'][0][1]).lower())
-        else:
-            # otherwise things are more complicated
-            min_l_dist = 1000000
-            matched_layer = None
-            for wms_layer in d[record_id]['wms_layers']:
-                record_title_norm = record_title.lower()
-                wms_layer_norm = (wms_layer[1]).lower()
-
-                l_dist = lvn.distance(record_title_norm, wms_layer_norm)
-                if l_dist < min_l_dist:
-                    min_l_dist = l_dist
-                    matched_layer = wms_layer_norm
-
-            if matched_layer is not None:
-                d[record_id]['wms_layer_for_record'] = matched_layer
-                d[record_id]['match_dist'] = min_l_dist
-
-    fn = "/home/james/Desktop/Work/matched_wms_layer_by_record_title.csv"
-    with open(fn, 'w') as outpf:
-        my_writer = csv.writer(outpf, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-        my_writer.writerow((['record_title', 'wms_layer_for_record', 'only_1_choice', 'match_dist']))
-        for record_id in d:
-            record_title = d[record_id]['record_title']
-            wms_layer_for_record = d[record_id]['wms_layer_for_record']
-            match_dist = d[record_id]['match_dist']
-            only_1_choice = d[record_id]['only_1_choice']
-            my_writer.writerow([record_title, wms_layer_for_record, only_1_choice, match_dist])
+                c += 1
+                if c < 200:
+                    title = r['title']
+                    subjects = r['subjects']
+                    url = r['url']
+                    wms_layer_for_record = r['wms_layer_for_record']
+                    only_1_choice = r['only_1_choice']
+                    match_dist = r['match_dist']
+                    wms_error = r['wms_error']
+                    print(c)
+                    print('title: ', title)
+                    print('subjects: ', subjects)
+                    print('url: ', url)
+                    print('wms_layer_for_record: ', wms_layer_for_record)
+                    print('only_1_choice: ', only_1_choice)
+                    print('match_dist: ', match_dist)
+                    print('wms_error: ', wms_error)
+                    print('\n')
 
 
 if __name__ == "__main__":
-    look_for_title_in_wms_layers()
-    #fetch_wms_layers()
+    main()
