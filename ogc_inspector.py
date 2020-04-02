a19666cbcf65a03b384e0371111e49ffea0a505b
jcrone <james.crone@ed.ac.uk>
2020-04-02 15:14:13 +0100

    bunch of refactoring

--- ogc_inspector.py
+++ ogc_inspector.py
@@ -1,118 +1,43 @@
 import csv
 import os
-import owslib
-from owslib.csw import CatalogueServiceWeb
-from owslib.fes import PropertyIsEqualTo, PropertyIsLike, BBox
-from owslib.wms import WebMapService
-from pyproj import Transformer
-import requests
 import uuid
-import owslib
-import logging
-import glob
-from PIL import Image
 import xml
-import pprint
-import Levenshtein as lvn
-
-# logging.basicConfig(
-#     filename='/home/james/geocrud/wms_getmaps/wms_getmap_log.txt',
-#     filemode='w',
-#     level=logging.DEBUG,
-#     format='%(levelname)s:%(message)s'
-# )
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import owslib
+from owslib.wms import WebMapService
+from PIL import Image
+import requests
+from shapely.geometry import box
 
 
-class MyBbox:
-    def __init__(self, xmin, ymin, xmax, ymax, srs):
-        self.xmin = xmin
-        self.ymin = ymin
-        self.xmax = xmax
-        self.ymax = ymax
-        self.srs = srs
+class UkRegions:
+    def __init__(self):
+        self.regions = {}
+        self._populate()
 
-    def __eq__(self, other):
-        return self.xmin == other.xmin and \
-               self.ymin == other.ymin and \
-               self.xmax == other.xmax and \
-               self.ymax == other.ymax and \
-               self.srs == other.srs
+    def _populate(self):
+        uk_region_extents = {
+            'northern_ireland': (-69.0, 466166.0, 177754.0, 610292.0),
+            'scotland': (5513.0, 530253.0, 470323.0, 1220302.0),
+            'england': (82672.0, 5338.0, 655605.0, 657534.0),
+            'wales': (146612.0, 164586.0, 355313.0, 395984.0),
+            'uk': (0.0, 0.0, 700000.0, 1300000.0)
+        }
 
+        for rgn in uk_region_extents:
+            extents = uk_region_extents[rgn]
+            bbox = box(extents[0], extents[1], extents[2], extents[3])
+            self.regions[rgn] = bbox
 
-def sub_from_bbox(xmin, ymin, xmax, ymax, buffer=1000):
-    centroid_x = (xmin + xmax) / 2.0
-    centroid_y = (ymin + ymax) / 2.0
-    bbox = (int(centroid_x), int(centroid_y), int(centroid_x + buffer), int(centroid_y + buffer))
-    return bbox
+    def get_region(self, region_name):
+        geom = None
+        if region_name in self.regions:
+            geom = self.regions[region_name]
 
+        return geom
 
-def bbox_to_bng(src_crs, bbox):
-    """
-    convert a bbox in some crs other than BNG to BNG
-
-    :param src_crs:
-    :param bbox
-    :return:
-    """
-    bbox_bng = None
-    if src_crs in (3857, 4326):
-        src_x_min, src_y_min, src_x_max, src_y_max = bbox[0], bbox[1], bbox[2], bbox[3]
-        transformer = Transformer.from_crs(src_crs, 27700)
-        bng_xy_min = transformer.transform(src_x_min, src_y_min)
-        bng_xy_max = transformer.transform(src_x_max, src_y_max)
-        bbox_bng = (int(bng_xy_min[0]), int(bng_xy_min[1]), int(bng_xy_max[0]), int(bng_xy_max[1]), 'EPSG:27700')
-
-    return bbox_bng
-
-
-def interrogate_wms_layer(wms_url, wms_lyr_name):
-    """
-    Interrogate/Test a WMS by making a GetMap request for an AOI of it`s buffered center
-
-    :param wms_url:
-    :param wms_lyr_name:
-    :return:
-    """
-    wms = WebMapService(wms_url)
-
-    lyr_bbox = wms[wms_lyr_name].boundingBox
-    if lyr_bbox[4] == 'EPSG:3857':
-        print("Converted layer BBOX from 3857 to 27700")
-        lyr_bbox = bbox_to_bng(3857, lyr_bbox)
-
-    uk_bbox = MyBbox(0.0, 0.0, 700000.0, 1300000.0, 'EPSG:27700')
-    oth_bbox = MyBbox(lyr_bbox[0], lyr_bbox[1], lyr_bbox[2], lyr_bbox[3], lyr_bbox[4])
-
-    if not oth_bbox == uk_bbox:
-        print(wms_url)
-        #print(wms.version)
-        #print(wms.identification.title)
-        #print(wms.identification.abstract)
-        print(wms_lyr_name)
-        #print(lyr_bbox)
-
-        sub_bbox = sub_from_bbox(lyr_bbox[0], lyr_bbox[1], lyr_bbox[2], lyr_bbox[3], buffer=1000)
-        #print(sub_bbox)
-        out_fn = os.path.join(
-            '/home/james/geocrud/wms_getmaps',
-            "".join([str(uuid.uuid1().int), "_wms_map.png"])
-        )
-
-        with open(out_fn, 'wb') as outpf:
-            try:
-                img = wms.getmap(
-                    layers=[wms_lyr_name],
-                    srs='EPSG:27700',
-                    bbox=sub_bbox,
-                    size=(400, 400),
-                    format='image/png'
-                    )
-                outpf.write(img.read())
-                logging.info("For WMS {} layer {}, wrote map to {}".format(wms_url, wms_lyr_name, out_fn))
-            except owslib.util.ServiceException as ex:
-                print("Exception generated when making GetMap request:")
-                print(ex)
-                logging.error("For WMS {} layer {}, had problems writing map to {}".format(wms_url, wms_lyr_name, out_fn))
+    def as_dict(self):
+        return self.regions
 
 
 def check_wms_map_image(fn):
@@ -127,85 +52,179 @@ def check_wms_map_image(fn):
                 status = "seems to all be background / no layer features in extent?"
         else:
             status = "seems to be a nosize img"
+    else:
+        status = "Image does not exist"
 
     return status
 
 
-def main():
+def interrogate_wms_layer(wms_url, wms_lyr_name, aoi_bbox):
+    wms_error = False
+    out_fn = None
+    try:
+        wms = WebMapService(wms_url)
+    except (owslib.util.ServiceException,
+            requests.RequestException, AttributeError, xml.etree.ElementTree.ParseError) as ex1:
+        wms_error = True
+        print(ex1)
+    else:
+        try:
+            img = wms.getmap(
+                layers=[wms_lyr_name],
+                srs='EPSG:27700',
+                bbox=aoi_bbox,
+                size=(400, 400),
+                format='image/png'
+                )
+        except owslib.util.ServiceException as ex2:
+            wms_error = True
+            print("Exception generated when making GetMap request:")
+            print(ex2)
+        else:
+            out_fn = os.path.join(
+                '/home/james/geocrud/wms_getmaps',
+                "".join([str(uuid.uuid1().int), "_wms_map.png"])
+            )
+            with open(out_fn, 'wb') as outpf:
+                outpf.write(img.read())
+
+    return wms_error, out_fn
+
+
+def fetch_bbox_for_wms_layers(in_fn='/home/james/geocrud/wms_layers.csv',
+                              out_fn='/home/james/geocrud/wms_layers_w_bbox.csv'):
+    c = 0
+
+    with open(out_fn, 'w') as outpf:
+        my_writer = csv.writer(outpf, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+        my_writer.writerow(['c', 'title', 'url', 'wms_layer_for_record', 'bb'])
+
+        if os.path.exists(in_fn):
+            with open(in_fn, 'r') as inpf:
+                my_reader = csv.DictReader(inpf)
+                for r in my_reader:
+                    c += 1
+                    title = r['title']
+                    subjects = r['subjects']
+                    url = r['url']
+                    wms_layer_for_record = r['wms_layer_for_record']
+                    only_1_choice = r['only_1_choice']
+                    match_dist = r['match_dist']
+                    wms_error = r['wms_error']
+                    if wms_error == 'False':
+                        bb = None
+                        try:
+                            wms = WebMapService(url, timeout=5)
+                        except owslib.util.ServiceException as ex_owslib:
+                            print('owslib exception: ', ex_owslib)
+                        except requests.exceptions.ReadTimeout as ex_req_rdto:
+                            print('Requests exception: ', ex_req_rdto)
+                        except requests.exceptions.RequestException as ex_req_reqex:
+                            print('Requests exception: ', ex_req_reqex)
+                        else:
+                            try:
+                                lyr = wms[wms_layer_for_record]
+                            except KeyError as ex:
+                                print(ex)
+                            else:
+                                bb = wms[wms_layer_for_record].boundingBox
+
+                        if bb is not None:
+                            my_writer.writerow([c, title, url, wms_layer_for_record, bb])
+
+
+def interrogate_wms_layers(in_fn='/home/james/geocrud/wms_layers_w_bbox.csv'):
     out_records = []
-    c = 0
-    in_fn = '/home/james/geocrud/wms_layers.csv'
-    out_fn = '/home/james/geocrud/wms_layers_w_bbox.csv'
 
-    # with open(out_fn, 'w') as outpf:
-    #     my_writer = csv.writer(outpf, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-    #     my_writer.writerow(['c', 'title', 'url', 'wms_layer_for_record', 'bb'])
-    #
-    #     if os.path.exists(in_fn):
-    #         with open(in_fn, 'r') as inpf:
-    #             my_reader = csv.DictReader(inpf)
-    #             for r in my_reader:
-    #                 c += 1
-    #                 title = r['title']
-    #                 subjects = r['subjects']
-    #                 url = r['url']
-    #                 wms_layer_for_record = r['wms_layer_for_record']
-    #                 only_1_choice = r['only_1_choice']
-    #                 match_dist = r['match_dist']
-    #                 wms_error = r['wms_error']
-    #                 if wms_error == 'False':
-    #                     #print('title: ', title)
-    #                     #print('subjects: ', subjects)
-    #                     #print('url: ', url)
-    #                     #print('wms_layer_for_record: ', wms_layer_for_record)
-    #                     #print('only_1_choice: ', only_1_choice)
-    #                     #print('match_dist: ', match_dist)
-    #                     #print('wms_error: ', wms_error)
-    #                     bb = None
-    #                     try:
-    #                         wms = WebMapService(url, timeout=5)
-    #                     except owslib.util.ServiceException as ex_owslib:
-    #                         print('owslib exception: ', ex_owslib)
-    #                     except requests.exceptions.ReadTimeout as ex_req_rdto:
-    #                         print('Requests exception: ', ex_req_rdto)
-    #                     except requests.exceptions.RequestException as ex_req_reqex:
-    #                         print('Requests exception: ', ex_req_reqex)
-    #                     else:
-    #                         try:
-    #                             lyr = wms[wms_layer_for_record]
-    #                         except KeyError as ex:
-    #                             pass
-    #                             #print(str(ex))
-    #                         else:
-    #                             bb = wms[wms_layer_for_record].boundingBox
-    #
-    #                     if bb is not None:
-    #                         my_writer.writerow([c, title, url, wms_layer_for_record, bb])
+    uk_regions = (UkRegions()).as_dict()
+    uk = uk_regions['uk']
 
     c = 0
-
     all_srs = {}
 
-    with open(out_fn, 'r') as inpf:
+    with open(in_fn, 'r') as inpf:
         my_reader = csv.DictReader(inpf)
-        with open('/home/james/Desktop/bng_wms_bboxes.csv', 'w') as outpf:
-            my_writer = csv.writer(outpf, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-            my_writer.writerow(['id', 'title', 'url', 'lyr_name', 'xmin', 'ymin', 'xmax', 'ymax', 'srs'])
-            for r in my_reader:
-                c += 1
+
+        for r in my_reader:
+            c += 1
+            if c < 10:
                 id = int(r['c'])
                 title = r['title']
                 url = r['url']
                 lyr_name = r['wms_layer_for_record']
                 lyr_bbox = ((r['bb'])[1:-1]).split(',')
-                xmin, ymin, xmax, ymax, srs = float(lyr_bbox[0]), float(lyr_bbox[1]), float(lyr_bbox[2]), float(lyr_bbox[3]), (lyr_bbox[4].replace("'", "")).strip()
+                xmin = float(lyr_bbox[0])
+                ymin = float(lyr_bbox[1])
+                xmax = float(lyr_bbox[2])
+                ymax = float(lyr_bbox[3])
+                srs = (lyr_bbox[4].replace("'", "")).strip()
+                print(c, "For Layer {}".format(title))
                 is_bng = False
                 if srs is not None:
                     if srs == 'EPSG:27700':
                         is_bng = True
 
                 if is_bng:
-                    my_writer.writerow([id, title, url, lyr_name, xmin, ymin, xmax, ymax, srs])
+                    wms_bbox = box(xmin, ymin, xmax, ymax)
+                    eq_uk_std = False
+
+                    print("\tBBox IS BNG".format(title))
+                    print("\tBBox area = {} km2".format(round(wms_bbox.area/1000000.0), 2))
+
+                    if wms_bbox.equals(uk_regions['uk']):
+                        eq_uk_std = True
+                        print("\t\tBBox IS equal to UK standard BBox".format(title))
+                    else:
+                        print("\t\tBBox IS NOT equal to UK standard BBox".format(title))
+                        print("\t\tIt however intersects with the following regions:")
+
+                        for rgn in uk_regions:
+                            if rgn != 'uk':
+                                rgn_g = uk_regions[rgn]
+                                if wms_bbox.intersects(rgn_g):
+
+                                    crude_comparision = round(((wms_bbox.area / rgn_g.area) * 100.0), 4)
+
+                                    print("\t\t\t {} ({}%)".format(rgn, crude_comparision))
+
+                    print("\tMaking GetMap() request using Layer BBox...")
+                    wms_error, out_fn = interrogate_wms_layer(
+                        wms_url=url,
+                        wms_lyr_name=lyr_name,
+                        aoi_bbox=wms_bbox.bounds
+                    )
+
+                    if not wms_error:
+                        image_status = None
+                        print("\t\tWrote GetMap image to {}".format(out_fn))
+                        image_status = check_wms_map_image(out_fn)
+                        print("\t\t{}".format(image_status))
+                    else:
+                        print("\t\tError making GetMap req/writing image")
+
+                    out_records.append([
+                        url,
+                        lyr_name,
+                        wms_error,
+                        image_status,
+                        out_fn
+                    ])
+
+                    print("\n")
+
+    if len(out_records) > 0:
+        env = Environment(
+            loader=FileSystemLoader('/home/james/PycharmProjects/mapcatalogue/templates'),
+            autoescape=select_autoescape(['html', 'xml'])
+        )
+        template = env.get_template('wms_validation_report_templ.html')
+        with open('/home/james/geocrud/wms_getmaps/wms_validation_report.html', 'w') as outpf:
+            outpf.write(template.render(my_list=out_records))
+
+
+def main():
+    interrogate_wms_layers()
+
 
 if __name__ == "__main__":
     main()
