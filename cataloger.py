bd12be67794741b17b86271c6bacfaabd0fa8c01
jcrone <james.crone@ed.ac.uk>
2020-04-15 12:36:28 +0100

    added bunch of debug prints

--- cataloger.py
+++ cataloger.py
@@ -32,6 +32,7 @@ def validate_bbox(src_bbox):
 
 def check_wms_map_image(fn):
     status = None
+    print("Checking image: ", fn)
 
     if os.path.exists(fn):
         if os.path.getsize(fn) > 0:
@@ -39,14 +40,16 @@ def check_wms_map_image(fn):
                 try:
                     im_colors_list = im.getcolors()
                 except TypeError as ex:
-                    status = "PIL problem"
-                    print(ex)
+                    print("Exception raised when checking image:")
+                    print('\t', ex)
+                    status = "Invalid"
                 else:
                     try:
                         number_of_cols_in_img = len(im_colors_list)
                     except TypeError as ex:
-                        status = "PIL problem"
-                        print(ex)
+                        print("Exception raised when checking image:")
+                        print('\t', ex)
+                        status = "Invalid"
                     else:
                         if number_of_cols_in_img > 1:
                             status = "seems to be populated"
@@ -58,6 +61,7 @@ def check_wms_map_image(fn):
     else:
         status = "Image does not exist"
 
+    print("Image Status is: ", status)
     return status
 
 
@@ -76,23 +80,36 @@ def search_ogc_service_for_record_title(ogc_url, record_title, wms_timeout=5):
     bbox = None
     bbox_srs = None
     match_dist = None
-    wms_error = False
+    wms_get_cap_error = False
+    wms_get_map_error = False
     made_get_map_req = False
     num_layers = 0
     image_status = None
     out_image_fname = None
 
+    print('WMS is: ', ogc_url)
+
     try:
         wms = WebMapService(ogc_url, timeout=wms_timeout)
-    except owslib.util.ServiceException as ex:
-        wms_error = True
-    except requests.RequestException as ex2:
-        wms_error = True
-    except AttributeError as ex3:
-        wms_error = True
-    except xml.etree.ElementTree.ParseError as ex4:
-        wms_error = True
+    except owslib.util.ServiceException as owslib_srv_ex:
+        print("Exception raised when instantiating WMS")
+        print('\t', owslib_srv_ex)
+        wms_get_cap_error = True
+    except requests.exceptions.RequestException as requests_ex:
+        print("Exception raised when instantiating WMS")
+        print('\t', requests_ex)
+        wms_get_cap_error = True
+    except AttributeError as attrib_error_ex:
+        print("Exception raised when instantiating WMS")
+        print('\t', attrib_error_ex)
+        wms_get_cap_error = True
+    except xml.etree.ElementTree.ParseError as etree_ex:
+        print("Exception raised when instantiating WMS")
+        print('\t', etree_ex)
+        wms_get_cap_error = True
     else:
+        print('WMS WAS instantiated OK')
+        print('Searching WMS layer that matches record_title: ', record_title)
         min_l_dist = 1000000
         matched_layer = None
         for wms_layer in wms.contents:
@@ -105,10 +122,12 @@ def search_ogc_service_for_record_title(ogc_url, record_title, wms_timeout=5):
                 matched_layer = wms_layer
 
         if matched_layer is not None:
+            print("Found matching WMS layer: ", matched_layer)
             wms_layer_for_record = matched_layer
             wms_layer_bbox = wms[wms_layer_for_record].boundingBox
             bbox_srs = wms_layer_bbox[4]
 
+            print("Attempting to make WMS GetMap request based on layer BBox")
             if bbox_srs != '':
                 match_dist = min_l_dist
                 made_get_map_req = True
@@ -120,13 +139,17 @@ def search_ogc_service_for_record_title(ogc_url, record_title, wms_timeout=5):
                         size=(400, 400),
                         format='image/png'
                         )
-                except owslib.util.ServiceException as owslib_ex:
-                    wms_error = True
-                    print(owslib_ex)
+                except owslib.util.ServiceException as owslib_srv_ex2:
+                    print("Exception raised when making WMS GetMap Request:")
+                    print('\t', owslib_srv_ex2)
+                    wms_get_map_error = True
                 except requests.exceptions.RequestException as requests_ex:
-                    wms_error = True
-                    print(requests_ex)
+                    print("Exception raised when making WMS GetMap Request:")
+                    print('\t', requests_ex)
+                    wms_get_map_error = True
                 else:
+                    print("GetMap request made OK")
+                    print("Writing map to temp image")
                     out_image_fname = os.path.join(
                         '/home/james/geocrud/wms_getmaps',
                         "".join([str(uuid.uuid1().int), "_wms_map.png"])
@@ -134,13 +157,14 @@ def search_ogc_service_for_record_title(ogc_url, record_title, wms_timeout=5):
                     with open(out_image_fname, 'wb') as outpf:
                         outpf.write(img.read())
 
+                    print("Checking written image")
                     if os.path.exists(out_image_fname):
                         image_status = check_wms_map_image(out_image_fname)
 
     if num_layers == 1:
         only_1_choice = True
 
-    return [wms_layer_for_record, bbox, bbox_srs, match_dist, only_1_choice, wms_error, made_get_map_req, image_status, out_image_fname]
+    return [wms_layer_for_record, bbox, bbox_srs, match_dist, only_1_choice, wms_get_cap_error, wms_get_map_error, made_get_map_req, image_status, out_image_fname]
 
 
 def get_ogc_type(url):
@@ -180,136 +204,137 @@ def query_csw(params):
     csw_url = params[0]
     start_pos = params[1]
     ogc_srv_type = params[2]
-    csw = CatalogueServiceWeb(csw_url)
-    csw.getrecords2(startposition=start_pos)
+    try:
+        csw = CatalogueServiceWeb(csw_url)
+    except (owslib.util.ServiceException, requests.exceptions.RequestException) as csw_ex:
+        print("Exception raised when instantiating CSW")
+        print('\t', csw_ex)
+    else:
+        csw.getrecords2(startposition=start_pos)
 
-    for rec in csw.records:
-        r = None
-        r = csw.records[rec]
+        for rec in csw.records:
+            r = None
+            r = csw.records[rec]
 
-        if r is not None:
-            # fetch / clean-up title
-            title = r.title
-            if title is not None:
-                title = title.replace("\n", "")
+            if r is not None:
+                # fetch / clean-up title
+                title = r.title
+                if title is not None:
+                    title = title.replace("\n", "")
 
-            # fetch / clean-up subjects
-            # convert the list of subjects to a string. Sometimes the list has a None, so filter these off
-            subjects = r.subjects
-            if subjects is not None:
-                subjects = ', '.join(list(filter(None, subjects)))
+                # fetch / clean-up subjects
+                # convert the list of subjects to a string. Sometimes the list has a None, so filter these off
+                subjects = r.subjects
+                if subjects is not None:
+                    subjects = ', '.join(list(filter(None, subjects)))
 
-            # fetch / clean-up references
-            references = r.references
-            if references is not None:
-                ogc_urls = []
-                for ref in references:
-                    url = ref['url']
-                    ogc_url_type = None
-                    if url is not None:
-                        ogc_url_type = get_ogc_type(url)
-                    if ogc_url_type is not None:
-                        if ogc_url_type == ogc_srv_type:
-                            # interogating WMS here
-                            res = search_ogc_service_for_record_title(url, title)
+                # fetch / clean-up references
+                references = r.references
+                if references is not None:
+                    ogc_urls = []
+                    for ref in references:
+                        url = ref['url']
+                        ogc_url_type = None
+                        if url is not None:
+                            ogc_url_type = get_ogc_type(url)
+                        if ogc_url_type is not None:
+                            if ogc_url_type == ogc_srv_type:
+                                # interogating WMS here
+                                res = search_ogc_service_for_record_title(url, title)
 
-                            wms_layer_for_record = res[0]
-                            if wms_layer_for_record is not None:
-                                bbox = res[1]
-                                bbox_srs = res[2]
-                                match_dist = res[3]
-                                only_1_choice = res[4]
-                                wms_error = res[5]
-                                made_get_map = res[6]
-                                image_status = res[7]
-                                out_image_fname = res[8]
-                                #print('wms_layer_for_record: ', wms_layer_for_record)
-                                #print('bbox: ', bbox, type(bbox))
-                                #print('bbox_srs: ', bbox_srs)
-                                #print('match_dist: ', match_dist)
-                                #print('only_1_choice: ', only_1_choice)
-                                #print('wms_error: ', wms_error)
-                                #print('made_get_map: ', made_get_map)
-                                #print('image_status: ', image_status)
-                                #print('\n')
-                                out_records.append([
-                                    title,
-                                    subjects,
-                                    url,
-                                    wms_layer_for_record,
-                                    only_1_choice,
-                                    match_dist,
-                                    bbox_srs,
-                                    wms_error,
-                                    made_get_map,
-                                    image_status,
-                                    out_image_fname
-                                ])
+                                wms_layer_for_record = res[0]
+                                if wms_layer_for_record is not None:
+                                    bbox = res[1]
+                                    bbox_srs = res[2]
+                                    match_dist = res[3]
+                                    only_1_choice = res[4]
+                                    wms_get_cap_error = res[5]
+                                    wms_get_map_error = res[6]
+                                    made_get_map = res[7]
+                                    image_status = res[8]
+                                    out_image_fname = res[9]
+                                    out_records.append([
+                                        title,
+                                        subjects,
+                                        url,
+                                        wms_layer_for_record,
+                                        only_1_choice,
+                                        match_dist,
+                                        bbox_srs,
+                                        wms_get_cap_error,
+                                        wms_get_map_error,
+                                        made_get_map,
+                                        image_status,
+                                        out_image_fname
+                                    ])
 
     return out_records
 
 
-def search_csw_for_ogc_endpoints(out_csv_fname, csw_url, limit_count=0, ogc_srv_type='WMS:GetCapabilties', debug=False):
+def search_csw_for_ogc_endpoints(out_csv_fname, csw_url, limit_count=0, ogc_srv_type='WMS:GetCapabilties'):
     limit_count = limit_count
-    csw = CatalogueServiceWeb(csw_url)
-    resultset_size = int(csw.constraints['MaxRecordDefault'].values[0])
+    try:
+        csw = CatalogueServiceWeb(csw_url)
+    except (owslib.util.ServiceException, requests.exceptions.RequestException) as csw_ex:
+        print("Exception raised when instantiating CSW")
+        print('\t', csw_ex)
+    else:
+        resultset_size = int(csw.constraints['MaxRecordDefault'].values[0])
 
-    if debug:
         print('CSW MaxRecordDefault:{}'.format(str(resultset_size)))
 
-    csw.getrecords2(startposition=0)
-    num_records = csw.results['matches']
+        csw.getrecords2(startposition=0)
+        num_records = csw.results['matches']
 
-    if debug:
         print('CSW Total Number of Matching Records:{}'.format(str(num_records)))
 
-    limited = False
-    if limit_count > 0:
-        limited = True
-        if limit_count < num_records:
-            num_records = limit_count
+        limited = False
+        if limit_count > 0:
+            limited = True
+            if limit_count < num_records:
+                num_records = limit_count
 
-    if debug:
         print('num_records: {} (limited:{})'.format(str(num_records), limited))
 
-    jobs = [[csw_url, i, ogc_srv_type] for i in range(0, num_records, resultset_size)]
+        jobs = [[csw_url, i, ogc_srv_type] for i in range(0, num_records, resultset_size)]
 
-    pool = ThreadPoolExecutor(max_workers=10)
+        pool = ThreadPoolExecutor(max_workers=10)
 
-    with open(out_csv_fname, 'w') as outpf:
-        my_writer = csv.writer(outpf, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-        out_fields = ['title', 'subjects', 'url', 'wms_layer_for_record', 'only_1_choice',
-                      'match_dist', 'bbox_srs', 'wms_error', 'made_get_map_req', 'image_status',
-                      'out_image_fname']
-        my_writer.writerow(out_fields)
+        with open(out_csv_fname, 'w') as outpf:
+            my_writer = csv.writer(outpf, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+            out_fields = [
+                'title',  # 0
+                'subjects',  # 1
+                'url',  # 2
+                'wms_layer_for_record',  # 3
+                'only_1_choice',  # 4
+                'match_dist',  # 5
+                'bbox_srs',  # 6
+                'wms_get_cap_error',  # 7
+                'wms_get_map_error',  # 8
+                'made_get_map_req',  # 9
+                'image_status',  # 10
+                'out_image_fname'  # 11
+            ]
+            my_writer.writerow(out_fields)
 
-        for job in pool.map(query_csw, jobs):
-            out_recs = job
-            if len(out_recs) > 0:
-                for r in out_recs:
-                    my_writer.writerow(r)
+            for job in pool.map(query_csw, jobs):
+                out_recs = job
+                if len(out_recs) > 0:
+                    for r in out_recs:
+                        my_writer.writerow(r)
 
 
 def generate_report(csv_fname='/home/james/Desktop/wms_layers.csv'):
     context = []
     if os.path.exists(csv_fname):
         with open(csv_fname, 'r') as inpf:
-            my_reader = csv.DictReader(inpf)
+            c = 1
+            my_reader = csv.reader(inpf)
             for r in my_reader:
-                title = r['title']
-                url = r['url']
-                lyr_name = r['wms_layer_for_record']
-                wms_error = r['wms_error']
-                image_status = r['image_status']
-                out_fn = r['out_image_fname']
-                context.append([
-                    title,
-                    url,
-                    lyr_name,
-                    wms_error,
-                    image_status,
-                    out_fn
-                ])
+                if c > 1:
+                    context.append(r)
+                c += 1
 
     env = Environment(
         loader=FileSystemLoader('/home/james/PycharmProjects/mapcatalogue/templates'),
